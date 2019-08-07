2c012440ae154457584babfdaec220a2ae7d6416
Chris Maughan <cmaughan@snarlsoftware.com>
2019-08-07 12:29:25 +0100

    js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// Adding an event listener lets us react to things happening in the browser
-// Here we listen for the "load" event, that tells us the page is ready for
-// us to run our scripts
 window.addEventListener("load", ()=> {
 
     // Start the animation
     move()
 })
 
-// Same as above, except we're listening for "click" events
 window.addEventListener("click",()=>{
     document.getElementById("maintext").style.color = "#0FF" 
 })
